a001df27a6455de5f7c1f0d77b91c8bb6c196878
sinilaa <emilia.laamanen@gmail.com>
2026-03-15 01:53:39 +0200

    Add comments

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 // Määritellään portti
 
+// Palauttaa tekstin selaimeen
 app.get('/', (req, res) => {
   res.send('Hello World! This is my Render app!')
 })
 
+// Käynnistetään palvelin ja kuunneellaan porttia
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
